0d14b2749cffc161de4b8b7f165ce15a244460ee
mprigent <77842628+mprigent@users.noreply.github.com>
2022-07-06 19:07:25 +0200

    Update microshell.c

--- microshell.c
+++ microshell.c
@@ -2,33 +2,33 @@
 #include <string.h>
 #include <sys/wait.h>
 
-void	ft_putstr_err(char *str)
+void ft_print_err(char *str)
 {
 	int i = 0;
 
 	while (str[i])
 		i++;
-	write(2, str, i);
+	write (2, str, i);
 }
 
-int	ft_execute(char **argv, int i, int tmp_fd, char **env)
+int ft_execute(char **argv, int i, int temp_fd, char **env)
 {
 	argv[i] = NULL;
-	close(tmp_fd);
+	close(temp_fd);
 	execve(argv[0], argv, env);
-	ft_putstr_err("error: cannot execute ");
-	ft_putstr_err(argv[0]);
-	write(2, "\n", 1);
+	ft_print_err("error : cannot execute ");
+	ft_print_err(argv[0]);
+	ft_print_err("\n");
 	return (1);
 }
 
-int	main(int argc, char **argv, char **env)
+int main(int argc, char **argv, char **env)
 {
 	(void)argc;
 	int fd[2];
 	int i = 0;
 	int pid = 0;
-	int tmp_fd = dup(STDIN_FILENO);
+	int temp_fd = dup(STDIN_FILENO);
 
 	while (argv[i] && argv[i + 1])
 	{
@@ -36,57 +36,56 @@ int	main(int argc, char **argv, char **env)
 		i = 0;
 		while (argv[i] && strcmp(argv[i], ";") && strcmp(argv[i], "|"))
 			i++;
-		if (strcmp(argv[0], "cd") == 0)
+		if (strcmp(argv[0], "cd") == 0) // cd errors
 		{
 			if (i != 2)
-				ft_putstr_err("error: cd: bad arguments\n");
+				ft_print_err("error: cd: bad arguments\n");
 			else if (chdir(argv[1]) != 0)
 			{
-				ft_putstr_err("error: cd: cannot change directory to ");
-				ft_putstr_err(argv[1]);
-				write(2, "\n", 1);
+				ft_print_err("error : cd: cannot change directory to ");
+				ft_print_err(argv[1]);
+				ft_print_err("\n");
 			}
 		}
-		else if (argv != &argv[i] && (argv[i] == NULL || strcmp(argv[i], ";") == 0))
+		else if (argv != &argv[i] && (argv[i] == NULL || strcmp(argv[i], ";") == 0)) // ; implementation
 		{
 			pid = fork();
 			if (pid == 0)
 			{
-				dup2(tmp_fd, STDIN_FILENO);
-				if (ft_execute(argv, i, tmp_fd, env))
+				dup2(temp_fd, STDIN_FILENO);
+				if (ft_execute(argv, i, temp_fd, env))
 					return (1);
 			}
 			else
 			{
-				close(tmp_fd);
+				close(temp_fd);
 				waitpid(-1, NULL, WUNTRACED);
-				tmp_fd = dup(STDIN_FILENO);
+				temp_fd = dup(STDIN_FILENO);
 			}
 		}
-		else if (argv != &argv[i] && strcmp(argv[i], "|") == 0)
+		else if (argv != &argv[i] && strcmp(argv[i], "|") == 0) // | implementation
 		{
-			pipe (fd);
+			pipe(fd);
 			pid = fork();
 			if (pid == 0)
 			{
-				dup2(tmp_fd, STDIN_FILENO);
+				dup2(temp_fd, STDIN_FILENO);
 				dup2(fd[1], STDOUT_FILENO);
 				close(fd[1]);
-				close(tmp_fd);
-				if (ft_execute(argv, i, tmp_fd, env))
+				close(temp_fd);
+				if (ft_execute(argv, i, temp_fd, env))
 					return (1);
 			}
 			else
 			{
-				close(tmp_fd);
+				close(temp_fd);
 				close(fd[1]);
 				waitpid(-1, NULL, WUNTRACED);
-				tmp_fd = dup(fd[0]);
+				temp_fd = dup(fd[0]);
 				close(fd[0]);
 			}
 		}
-
 	}
-	close(tmp_fd);
+	close(temp_fd);
 	return (0);
 }
